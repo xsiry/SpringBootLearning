06b958359051a95b504f9951605624e971123a2d
xsiry <xsiries@gmail.com>
2018-01-18 16:22:13 +0800

    #SpringBoot初始搭建

---
+++ src/main/java/com/x/proc/ProcApplication.java
@@ -0,0 +1,13 @@
+package com.x.proc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Spring Boot 应用的标识
+@SpringBootApplication
+public class ProcApplication {
+	// 程序启动入口
+	// 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+	public static void main(String[] args) {
+		SpringApplication.run(ProcApplication.class, args);
+	}
+}
