29bf836104818695c6aeebfb99ea00ec184c77a3
xsiry <xsiries@gmail.com>
2018-04-26 17:24:17 +0800

    #商品管理

--- src/main/resources/static/app/index/src/menus_data.js
+++ src/main/resources/static/app/index/src/menus_data.js
@@ -1,8 +1,8 @@
 define({ "Total": 2, "Rows":
 	[
-		{ "id": 5, "name": "员工管理", "text": "员工管理", "href": "app/user/user.html", "icon": "xi xi-swap", "level": 1, "children": [] },
-        { "id": 6, "name": "厂商管理", "text": "厂商管理", "href": "app/factory/factory.html", "icon": "xi xi-download", "level": 1, "children": [] },
-        { "id": 2, "name": "产品管理", "text": "产品管理", "href": "app/product/product.html", "icon": "xi xi-menu", "level": 1, "children": [] },
-		{ "id": 7, "name": "员工月结", "text": "员工月结", "href": "app/game.html", "icon": "xi xi-apps", "level": 1, "children": [] }
+		{ "id": 5, "name": "员工管理", "text": "员工管理", "href": "app/user/user.html", "icon": "xi xi-accounts", "level": 1, "children": [] },
+        { "id": 6, "name": "厂商管理", "text": "厂商管理", "href": "app/factory/factory.html", "icon": "xi xi-local-shipping", "level": 1, "children": [] },
+        { "id": 2, "name": "产品管理", "text": "产品管理", "href": "app/product/product.html", "icon": "xi xi-shopping-cart", "level": 1, "children": [] },
+		{ "id": 7, "name": "员工月结", "text": "员工月结", "href": "app/game.html", "icon": "xi xi-calendar-note", "level": 1, "children": [] }
 	]
 })
